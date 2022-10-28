20199818116ba76db924300da83834c4700a314b
Afiq139 <shafiq5427.engineering@gmail.com>
2022-10-28 15:31:55 +0800

    Update sandbox.ts

--- Typescript-Project/Session_1/10_Function_Signatures/src/sandbox.ts
+++ Typescript-Project/Session_1/10_Function_Signatures/src/sandbox.ts
@@ -3,3 +3,10 @@
 //just type tsc -w after done this inside tsconfig.json
 //"rootDir": "./src",      
 // "outDir": "./public",   
+
+//example 1
+let greet: (a: string, b:string) => void;
+
+greet = (name: string, greeting: string) => {
+    console.log('${name} says ${greeting}');
+}
