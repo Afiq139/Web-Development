24bf94c80b51d1a24f9d596ceb37ad6417b95763
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-01-19 19:58:45 +0800

    update

--- pythonanywhere-Project/demo_project/dictionary_app/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from dictionary_app.models import Word
-
-class WordForm(forms.ModelForm):
-    field_order = ['word', 'meaning']
-    class Meta():
-        model = Word
-        fields = ('word','meaning')
