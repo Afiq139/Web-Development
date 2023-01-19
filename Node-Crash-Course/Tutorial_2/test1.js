00a35283e55711e00ccf143a659ff11e39f4a4f0
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-01-19 23:44:26 +0800

    update

--- Node-Crash-Course/Tutorial_2/test1.js
+++ Node-Crash-Course/Tutorial_2/test1.js
@@ -1,9 +1,16 @@
+console.log('----------------------------');
+console.log('Introduction --- 0.00');
+console.log('----------------------------');
 const test = 'mario';
 console.log(test); //use node test1 to run
-
+console.log('');
 const greet = (name) => {
     console.log(`hello, ${name}`);
 }
 
 greet('mario');
 greet('yoshi');
+
+console.log('----------------------------');
+console.log('The Global Object --- 2.09');
+console.log('----------------------------');
