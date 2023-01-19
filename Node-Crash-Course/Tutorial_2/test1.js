477fe8795c8933c57bd220d0c11791e956327ca2
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-01-19 23:56:30 +0800

    Update test1.js

--- Node-Crash-Course/Tutorial_2/test1.js
+++ Node-Crash-Course/Tutorial_2/test1.js
@@ -14,4 +14,17 @@ greet('yoshi');
 console.log('----------------------------');
 console.log('The Global Object --- 2.09');
 console.log('----------------------------');
+//type 'clear' to clear the console
+
+//console.log(global);
+
+// global.setTimeout(() => {
+//     console.log('In the timeout');
+// }, 3000); //3s
+
+setTimeout(() => {
+    console.log('In The timeout');
+}, 3000);
+
+
 
