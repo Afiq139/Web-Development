8003a2432dce7387e3fda7f2bc159da442b25037
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-01-24 00:57:05 +0800

    Update server.js

--- Node-Crash-Course/Tutorial_3/server.js
+++ Node-Crash-Course/Tutorial_3/server.js
@@ -5,7 +5,7 @@ const http = require('http');
 //can store like in websocket
 // request object(req) and response object(res)
 const server = http.createServer(() => {
-    console.log('request made');
+    console.log('request made'); //running in server (backend), not in browser
 }); 
 
 server.listen(3000, 'localhost', () => {
@@ -19,3 +19,8 @@ server.listen(3000, 'localhost', () => {
 
 //localhost is like a domain name on the web
 // localhost ---> 127.0.0.1 ---> Own Computer
+//port number are like 'doors' into a computer
+
+//run server with 'node server'  
+//go to 'localhost:3000'
+//ctrl + c to cancel the server running process
