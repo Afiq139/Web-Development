b7f0781912109dcc108ec65a0872a2909afd6100
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-01-24 00:41:11 +0800

    Create server.js

---
+++ Node-Crash-Course/Tutorial_3/server.js
@@ -0,0 +1,6 @@
+//get http module
+const http = require('http'); 
+
+//create server using createServer method and store it in server
+//can store like in websocket
+const server = http.createServer(); 
