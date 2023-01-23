61b7a45e9c12e6321d6d9afdf5f269c7812120d0
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-01-24 00:49:32 +0800

    Update server.js

--- Node-Crash-Course/Tutorial_3/server.js
+++ Node-Crash-Course/Tutorial_3/server.js
@@ -1,6 +1,21 @@
 //get http module
 const http = require('http'); 
 
-//create server using createServer method and store it in server
+//create server using createServer method and store it in server const
 //can store like in websocket
-const server = http.createServer(); 
+// request object(req) and response object(res)
+const server = http.createServer(() => {
+    console.log('request made');
+}); 
+
+server.listen(3000, 'localhost', () => {
+    console.log('listening for request on port 3000');
+});
+
+
+//----------------------------
+//Localhost & Port Numbers --- 8.52
+//----------------------------
+
+//localhost is like a domain name on the web
+// localhost ---> 127.0.0.1 ---> Own Computer
