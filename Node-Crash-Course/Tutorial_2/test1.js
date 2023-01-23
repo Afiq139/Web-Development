c99f635791b827ecc65b8baa784f5c10eed690d0
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-01-23 23:40:50 +0800

    update

--- Node-Crash-Course/Tutorial_2/test1.js
+++ Node-Crash-Course/Tutorial_2/test1.js
@@ -100,3 +100,11 @@ fs.mkdir('./assets', (err) => {
 }
 
 //----------------deleting files----------------
+if(fs.existsSync('./docs/deleteme.txt')){
+    fs.unlink('./docs/deleteme.txt', (err) => {
+        if (err) {
+            console.log(err);
+    }
+    console.log('File deleted');
+    })
+}
