511cff072b89eb5a391c9540bc6ba1b3665f8102
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-01-25 03:38:02 +0800

    Update server.js

--- Node-Crash-Course/Tutorial_4/server.js
+++ Node-Crash-Course/Tutorial_4/server.js
@@ -1,24 +1,24 @@
-//----------------------------
+//---------------------------------------------------------------
 //Request Object --- 0.00
-//----------------------------
+//---------------------------------------------------------------
 //get http module
-const http = require('http'); 
+// const http = require('http'); 
 
 //create server using createServer method and store it in server const
 //can store like in websocket
 // request object(req) and response object(res)
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method); //running in server (backend), not in browser
-}); 
+// const server = http.createServer((req, res) => {
+//     console.log(req.url, req.method); //running in server (backend), not in browser
+// }); 
 
-server.listen(3000, 'localhost', () => {
-    console.log('listening for request on port 3000');
-});
+// server.listen(3000, 'localhost', () => {
+//     console.log('listening for request on port 3000');
+// });
 
 
-//----------------------------
+//---------------------------------------------------------------
 //The Response Object --- 3.23
-//----------------------------
+//---------------------------------------------------------------
 
 //localhost is like a domain name on the web
 // localhost ---> 127.0.0.1 ---> Own Computer
@@ -27,3 +27,45 @@ server.listen(3000, 'localhost', () => {
 //run server with 'node server'  
 //go to 'localhost:3000'
 //ctrl + c to cancel the server running process
+// const http = require('http'); 
+
+// const server = http.createServer((req, res) => {
+//     console.log(req.url, req.method); //running in server (backend), not in browser
+
+//     //set header content type
+//     //res.setHeader('Content-Type', 'text/plain');
+//     res.setHeader('Content-Type', 'text/html');
+    
+
+//     res.write('<head><link rel="stylesheet" href="#"></head>');
+//     res.write('<p> Hello shafiq</p>');
+//     res.write('<p> How are you </p>');
+//     res.end();
+// }); 
+
+// server.listen(3000, 'localhost', () => {
+//     console.log('listening for request on port 3000');
+// });
+
+//---------------------------------------------------------------
+//Returning HTML Pages --- 9.07
+//---------------------------------------------------------------
+const http = require('http'); 
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method); //running in server (backend), not in browser
+
+    //set header content type
+    //res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'text/html');
+    
+
+    res.write('<head><link rel="stylesheet" href="#"></head>');
+    res.write('<p> Hello shafiq</p>');
+    res.write('<p> How are you </p>');
+    res.end();
+}); 
+
+server.listen(3000, 'localhost', () => {
+    console.log('listening for request on port 3000');
+});
