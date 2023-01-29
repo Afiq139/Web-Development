c8ea65d9daa77762ea35a27a3fb780e0f3b1953e
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-01-29 15:45:25 +0800

    update

--- Node-Crash-Course/Tutorial_6_ Express-Apps/app.js
+++ Node-Crash-Course/Tutorial_6_ Express-Apps/app.js
@@ -11,5 +11,33 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.send('<p> Home Page </p>');  //susbstitute the res.write and res.end()
+    //res.send('<p> Home Page </p>');  //susbstitute the res.write and res.end()
+    res.sendFile('./views/index.html', {root:__dirname});
   })
+
+
+//---------------------------------------------------------------
+//Routing & HTML --- 7.21
+//---------------------------------------------------------------
+
+app.get('/about', (req, res) => {
+    //res.send('<p>About Page </p>');  //susbstitute the res.write and res.end()
+    res.sendFile('./views/about.html', {root:__dirname});
+  })
+
+
+//---------------------------------------------------------------
+//Redirect & 404's --- 11.26
+//---------------------------------------------------------------
+
+//redirect
+app.get('/about-us', (req, res) => {
+    res.redirect('/about'); 
+})
+
+//404 page (.use() ---> create middleware and fire it to all)
+//also must most bellow of script
+//if put the script in middle code, it'll not running others below it
+app.use((req, res) => {
+    res.status(404).sendFile('./views/404.html', {root:__dirname});
+});
