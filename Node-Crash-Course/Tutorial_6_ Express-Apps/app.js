152c5e83530e3fffa9d083ffe74c98bb993ae00c
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-01-29 15:19:54 +0800

    update

--- Node-Crash-Course/Tutorial_6_ Express-Apps/app.js
+++ Node-Crash-Course/Tutorial_6_ Express-Apps/app.js
@@ -1,3 +1,15 @@
 //---------------------------------------------------------------
 //Creating an Express App--- 2.27
 //---------------------------------------------------------------
+
+const express = require('express');
+
+//express app
+const app = express();
+
+//listen for request
+app.listen(3000);
+
+app.get('/', (req, res) => {
+    res.send('<p> Home Page </p>');  //susbstitute the res.write and res.end()
+  })
