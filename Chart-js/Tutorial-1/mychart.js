2a90f4721af5884ee533430b0d3425e21b4ee4de
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-02-05 23:35:33 +0800

    Update mychart.js

--- Chart-js/Tutorial-1/mychart.js
+++ Chart-js/Tutorial-1/mychart.js
@@ -1,11 +1,13 @@
 const ctx = document.getElementById('myChart');
       
 new Chart(ctx, {
-  type: 'line', //line, bar, radar, doughnut & Pie, polar Area, Bubble, Scatter, Area, Mixed and so on
+  type: 'line', //line, bar, radar, doughnut & Pie, Bubble, Scatter, Area, Mixed and so on
   data: {
     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
     datasets: [{
       label: '# of Votes',
+      backgroundColor: 'rgba(255, 99, 132)',
+      borderColor: 'rgba(255, 99, 132)',
       data: [12, 19, 3, 5, 2, 3],
       borderWidth: 1
     }]
