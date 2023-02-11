09d92225373ff9ebb53020c0c0a153ffdb081cb2
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-02-11 12:40:27 +0800

    Update mychart.js

--- Chart-js/Tutorial-1/mychart.js
+++ Chart-js/Tutorial-1/mychart.js
@@ -7,18 +7,18 @@ var beginnerchart = document.getElementById('beginnerchart').getContext('2d');
 var chart = new Chart(beginnerchart, {
 
   // The type of chart we want to create
-  type: 'line', 
+  type: 'bar', 
   //line, bar, radar, doughnut & Pie, Bubble, Scatter, Area, Mixed and so on
 
 
   //the data for our dataset
   data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', 'Black'],
     datasets: [{
       label: '# of Votes',
-      backgroundColor: 'rgba(255, 99, 132)',
+      backgroundColor: ['rgb(255, 99, 132)', 'rgba(255, 0, 0, 0.1)', 'gray', '#009900'],
       borderColor: 'rgba(255, 99, 132)',
-      data: [12, 19, 3, 5, 2, 3],
+      data: [12, 19, 3, 5, 2, 3, 10],
       borderWidth: 1
     }]
   },
