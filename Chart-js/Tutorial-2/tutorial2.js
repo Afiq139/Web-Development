8c06686f45c7b0c0e7bb9fdb4e5f98cfdacda98a
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-02-22 15:46:16 +0800

    Update tutorial2.js

--- Chart-js/Tutorial-2/tutorial2.js
+++ Chart-js/Tutorial-2/tutorial2.js
@@ -11,15 +11,35 @@ blankcanvas.fillStyle = "#FF0000";
 
 //blankcanvas.fillRect(0, 0, 150, 75);
 
-blankcanvas.fillRect(0, 10, 300, 1);
-blankcanvas.fillRect(0, 20, 300, 1);
-blankcanvas.fillRect(0, 30, 300, 1);
-blankcanvas.fillRect(0, 40, 300, 1);
-blankcanvas.fillRect(0, 50, 300, 1);
-blankcanvas.fillRect(0, 60, 300, 1);
+// blankcanvas.fillRect(0, 10, 300, 1);
+// blankcanvas.fillRect(0, 20, 300, 1);
+// blankcanvas.fillRect(0, 30, 300, 5);
+// blankcanvas.fillRect(0, 40, 300, 1);
+// blankcanvas.fillRect(0, 50, 300, 1);
+// blankcanvas.fillRect(0, 60, 300, 1);
 
 
-blankcanvas.fillRect(10, 0, 1, 150);
-blankcanvas.fillRect(20, 0, 1, 150);
-blankcanvas.fillRect(30, 0, 1, 150);
-blankcanvas.fillRect(40, 0, 1, 150);
+// blankcanvas.fillRect(10, 0, 1, 150);
+// blankcanvas.fillRect(20, 0, 1, 150);
+// blankcanvas.fillRect(30, 0, 5, 150);
+// blankcanvas.fillRect(40, 0, 1, 150);
+// blankcanvas.fillRect(50, 0, 1, 150);
+
+
+
+blankcanvas.fillRect(10, 140, 300, 5);
+blankcanvas.fillRect(10, 130, 300, 1);
+blankcanvas.fillRect(10, 120, 300, 1);
+blankcanvas.fillRect(10, 110, 300, 1);
+blankcanvas.fillRect(10, 100, 300, 1);
+blankcanvas.fillRect(10, 90, 300, 1);
+
+
+blankcanvas.fillRect(10, 10, 5, 130);
+blankcanvas.fillRect(20, 10, 1, 130);
+blankcanvas.fillRect(30, 10, 1, 130);
+blankcanvas.fillRect(40, 10, 1, 130);
+
+blankcanvas.fillStyle = "#00FF00";
+blankcanvas.fillRect(20, 90, 10, 50);
+blankcanvas.fillRect(40, 50, 10, 90);
