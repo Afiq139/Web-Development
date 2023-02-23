deeda594eb4c3cc220c9b13c78a7f68be97948e8
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-02-23 08:55:16 +0800

    Update tutorial3.js

--- Chart-js/Tutorial-3/tutorial3.js
+++ Chart-js/Tutorial-3/tutorial3.js
@@ -3,19 +3,23 @@
 const firsthalf = [2, 19, 3, 5, 2, 3];
 const secondhalf = [15, 20, 25, 10, 20, 15];
 
+const firsttMonth = ['jan', 'fab', 'mar', 'apr', 'may', 'jun'];
+const secondMonth = ['jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
 
-
+//combine array
+const year = firsthalf.concat(secondhalf);
+console.log(year);
 
 
 const barchart = document.getElementById('barchart').getContext('2d');
 const mychart = new Chart(barchart, {
   type: 'bar',
   data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: firsttMonth,
     datasets: [{
       label: '# of Votes',
     //   data: [12, 19, 3, 5, 2, 3],
-      data: secondhalf,
+      data: year,
       backgroundColor: [
         'rgba(255, 99, 132, 0.2)',
         'rgba(54, 162, 235, 0.2)',
