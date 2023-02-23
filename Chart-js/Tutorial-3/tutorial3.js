a58d3a23b1d6beb4513da25ca2074b5fb6f92f63
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-02-23 08:46:48 +0800

    Update tutorial3.js

--- Chart-js/Tutorial-3/tutorial3.js
+++ Chart-js/Tutorial-3/tutorial3.js
@@ -1,3 +1,12 @@
+
+//Array
+const firsthalf = [2, 19, 3, 5, 2, 3];
+const secondhalf = [15, 20, 25, 10, 20, 15];
+
+
+
+
+
 const barchart = document.getElementById('barchart').getContext('2d');
 const mychart = new Chart(barchart, {
   type: 'bar',
@@ -5,14 +14,25 @@ const mychart = new Chart(barchart, {
     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
     datasets: [{
       label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      bacgroundColor: [
+    //   data: [12, 19, 3, 5, 2, 3],
+      data: secondhalf,
+      backgroundColor: [
         'rgba(255, 99, 132, 0.2)',
         'rgba(54, 162, 235, 0.2)',
         'rgba(255, 206, 86, 0.2)',
         'rgba(75, 192, 192, 0.2)',
         'rgba(153, 102, 255, 0.2)',
         'rgba(255, 159, 64, 0.2)',
+
+      ],
+      borderColor:[
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+
 
       ],
 
