527d906db897d9fc61e1b49d893d5239a11aa5e9
Shafiq-Engineering <shafiq5427.engineering@gmail.com>
2023-03-01 21:14:54 +0800

    Update tutorial5.js

--- Chart-js/Tutorial-5/tutorial5.js
+++ Chart-js/Tutorial-5/tutorial5.js
@@ -15,7 +15,7 @@ const mychart = new Chart(ctx, {
         'rgba(255, 206, 86, 0.5)',
         'rgba(75, 192, 192, 0.5)',
         'rgba(153, 102, 255, 0.6)',
-        'rgba(255, 159, 64, 0.5)',
+        'rgba(255, 159, 64, 0.4)',
     
       ],
       borderColor: [
